2a82bf768c179ba189fbc92a30b94ca05d9c6e98
Student <student@thunder-2.local>
2016-04-07 12:12:47 -0500

    Added two view controllers, one for the sound view controller (SoundViewController) and one for the choices (ChoicesViewController) Added two buttons, one pause button on the SoundViewController and ChoicesViewController

---
+++ What Was That? Final/SoundViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SoundViewController.swift
+//  What Was That? Final
+//
+//  Created by Student on 4/7/16.
+//  Copyright © 2016 The Coolest Guy Around. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func pauseGameOnTapped(sender: AnyObject)
+    {
+        
+    }
+
+}
