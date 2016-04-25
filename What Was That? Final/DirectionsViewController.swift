4203d8e461655171592fcd004055ca6543ee3ff7
Student <student@4cool20guy.local>
2016-04-25 12:17:58 -0500

    Adjusted Directions and fixed Modal

---
+++ What Was That? Final/DirectionsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DirectionsViewController.swift
+//  What Was That? Final
+//
+//  Created by Student on 4/25/16.
+//  Copyright © 2016 The Coolest Guy Around. All rights reserved.
+//
+
+import UIKit
+
+class DirectionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func onDoneTapped(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
