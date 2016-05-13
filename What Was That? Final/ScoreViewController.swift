70793095528ee087716d6da59c54687ba09d1d05
Amy Barcroft <abarcroft@gmail.com>
2016-05-13 13:12:39 -0500

    Added a score board.

--- What Was That? Final/ScoreViewController.swift
+++ What Was That? Final/ScoreViewController.swift
@@ -9,27 +9,15 @@
 import UIKit
 
 class ScoreViewController: UIViewController {
-
+    @IBOutlet weak var scoreValue: UILabel!
+    
+    var scoreNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        scoreValue.text = "\(scoreNumber)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
